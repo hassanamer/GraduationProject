f9839cd051b1389a28652518dd0f5659812c4228
Hassan Amer <51220231+hassanamer@users.noreply.github.com>
2022-03-16 16:36:35 +0200

    App Colors

---
+++ lib/shared/styles/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff005764);
+  static const Color backgroundColor = Color(0xffffffff);
+  static const Color buttonTextColor = Color(0xffffffff);
+  static const Color textFieldColor = Color(0xffbec5d1);
+  static const Color buttonTwoColor = Color(0xffc1d4d6);
+  static const Color disabledAndHintColor = Color(0xff9ca3af);
+
+}
