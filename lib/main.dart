aed345a0b95c06ac6d7e706b6fbbedf17b714ca1
Hassan Amer <51220231+hassanamer@users.noreply.github.com>
2022-03-21 20:00:33 +0200

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smarttouristguide/modules/login/success_screens/sucsses_screen2.dart';
+import 'package:smarttouristguide/modules/login/success_screens/check_mail_success.dart';
 
 import 'modules/home/home_screen.dart';
 
