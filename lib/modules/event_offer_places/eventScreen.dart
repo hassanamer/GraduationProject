56d6c9c42fbda645e0a15b8db17753b635a7cb54
Abeer Mahmoud <68340943+abeer582@users.noreply.github.com>
2022-04-02 13:26:56 -0700

    navigation

--- lib/modules/event_offer_places/eventScreen.dart
+++ lib/modules/event_offer_places/eventScreen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+
 import 'ListVievOfEventAndOffer.dart';
 class EventScreen extends StatefulWidget {
   @override
   State<EventScreen> createState() => _EventScreenState();
 }
-
 class _EventScreenState extends State<EventScreen> {
   late int currentScreenIndex ;
   @override
@@ -21,41 +21,40 @@ class _EventScreenState extends State<EventScreen> {
         backgroundColor: Theme.of(context).primaryColor,
         actions: [IconButton(onPressed: (){}, icon: Icon(Icons.search))],
       ),
-      body: ListVievOfEventAndOffer(),
-      bottomNavigationBar:Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                topRight: Radius.circular(20), topLeft: Radius.circular(20)),
-            boxShadow: [
-              BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
-            ],
+      body: ListVievOfEventAndOffer(), bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
           ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
-            child: BottomNavigationBar(
-              backgroundColor: Colors.white,
-              currentIndex: currentScreenIndex,
-              onTap: (index){
-                setState(() {
-                  currentScreenIndex=index;
-                });
-              },
-              showUnselectedLabels: false,
-              showSelectedLabels: true,
-              selectedItemColor: Theme.of(context).primaryColor ,
-              unselectedItemColor: Theme.of(context).colorScheme.inversePrimary,
-              type: BottomNavigationBarType.fixed ,
-              items: [
-                BottomNavigationBarItem(icon: Icon(Icons.home,size: 30,),label: ("Home")),
-                BottomNavigationBarItem(icon: Icon(Icons.favorite,size: 30),label: ("favorite")),
-                BottomNavigationBarItem(icon: Icon(Icons.category,size: 30),label: ("Category"))
-              ],),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            currentIndex: currentScreenIndex,
+            onTap: (index){
+              setState(() {
+                currentScreenIndex=index;
+              });
+            },
+            showUnselectedLabels: false,
+            showSelectedLabels: true,
+            selectedItemColor: Theme.of(context).primaryColor ,
+            unselectedItemColor: Theme.of(context).colorScheme.inversePrimary,
+            type: BottomNavigationBarType.fixed ,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home,size: 30,),label: ("Home")),
+              BottomNavigationBarItem(icon: Icon(Icons.favorite,size: 30),label: ("favorite")),
+              BottomNavigationBarItem(icon: Icon(Icons.category,size: 30),label: ("Category"))
+            ],),
 
-          )
-      ));
+        ))
+    );
   }
 }
 
--- lib/modules/login/login_and_signup/welcomScreen.dart
+++ lib/modules/login/login_and_signup/welcomScreen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:smarttouristguide/shared/styles/buttons_style.dart';
 
 import 'LoginButton.dart';
 import 'SignUpButton.dart';
+import 'SignUpScreen.dart';
+import 'login_screen.dart';
 
 class Welcome extends StatelessWidget {
-
+  static const routeName = 'welcom';
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
@@ -35,8 +38,37 @@ class Welcome extends StatelessWidget {
                   ),
                 ),
               ),
-              SignUpButton(),
-              LoginButton()
+              buttons(function: (){
+                showModalBottomSheet(
+                    isDismissible: false,
+                    enableDrag: false,
+                    backgroundColor: Colors.transparent,
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                          height:double.infinity,decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+                          color: Colors.white) ,
+                          child:SignUpScreen());
+                    });
+              }, text: 'Sign Up'),
+             buttons(function: (){{
+               showModalBottomSheet(
+                   isDismissible: false,
+                   enableDrag: false,
+                   backgroundColor: Colors.transparent,
+                   context: context,
+                   builder: (context) {
+                     return Container(
+                         height:double.infinity,decoration: BoxDecoration(
+                         borderRadius: BorderRadius.only(
+                             topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+                         color: Colors.white) ,
+                         child:
+                         LoginScreen());
+                   });
+             }}, text: 'Login')
               // buttons('Login', DoFunction)
             ],
           ),
