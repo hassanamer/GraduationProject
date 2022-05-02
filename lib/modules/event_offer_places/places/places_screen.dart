5ce5c89016a311fe673fed371d1dc25e9df1acab
hazemosama <100768629+hazemosama@users.noreply.github.com>
2022-05-02 19:22:12 +0200

    شوية تظبيطات

--- lib/modules/event_offer_places/places/places_screen.dart
+++ lib/modules/event_offer_places/places/places_screen.dart
@@ -7,15 +7,25 @@ class PlacesScreen extends StatelessWidget {
   static const String routeName = 'PlacesScreen';
 
   const PlacesScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Places'),
+        title: const Text(
+          'Places',
+        ),
         backgroundColor: AppColors.primaryColor,
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+            ),
+          ),
+        ],
       ),
       body: ListViewOfPlaces(),
     );
