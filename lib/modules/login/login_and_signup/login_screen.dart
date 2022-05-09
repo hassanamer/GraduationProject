d75b15e3319cd8b4f1213ced8d2e862dc514428c
hazemosama <100768629+hazemosama@users.noreply.github.com>
2022-05-09 16:43:49 +0200

    more advanced API

--- lib/modules/login/login_and_signup/login_screen.dart
+++ lib/modules/login/login_and_signup/login_screen.dart
@@ -1,20 +1,17 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
-import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:smarttouristguide/layout/app_layout.dart';
 import 'package:smarttouristguide/modules/login/login_and_signup/forget_password.dart';
 import 'package:smarttouristguide/modules/login/login_cubit/cubit.dart';
 import 'package:smarttouristguide/modules/login/login_cubit/states.dart';
 import 'package:smarttouristguide/shared/network/local/cache_helper.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:smarttouristguide/shared/styles/textStyle.dart';
-
+import 'package:smarttouristguide/shared/styles/colors.dart';
 import '../../../shared/components/components.dart';
-import '../../../shared/styles/buttons_style.dart';
 
 class LoginScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     var cubit = AppLoginCubit.get(context);
@@ -23,33 +20,28 @@ class LoginScreen extends StatelessWidget {
 
     return BlocConsumer<AppLoginCubit, AppLoginStates>(
       listener: (context, state) {
-        if (state is AppLoginSuccessState) {
-          if (state.loginModel!.status) {
+        if (state is AppLoginSuccessState)
+        {
+          if (state.loginModel!.status)
+          {
             CacheHelper.saveData(
               key: 'token',
               value: state.loginModel!.data.token,
             ).then((value) {
+              cubit.getToken();
+
               navigateAndFinish(
                 context: context,
                 widget: AppLayout(),
               );
             });
-            Fluttertoast.showToast(
-              msg: "${state.loginModel!.message}",
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.BOTTOM,
-              timeInSecForIosWeb: 5,
-              backgroundColor: Colors.green,
-              textColor: Colors.white,
-              fontSize: 16.0,
-            );
-          } else {
             showToast(
               message: AppLocalizations.of(context)!.login_success,
-              state: ToastStates.ERROR,
+              state: ToastStates.SUCCESS,
             );
+          } else {
           }
-        } else if (state is AppLoginErrorState) {
+        } else if(state is AppLoginErrorState) {
           showToast(
             message: AppLocalizations.of(context)!.login_error,
             state: ToastStates.ERROR,
@@ -57,91 +49,88 @@ class LoginScreen extends StatelessWidget {
         }
       },
       builder: (context, state) {
-        return Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(30),
-                topLeft: Radius.circular(30),
-              )),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-              key: cubit.formKey,
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  defaultFormField(
-                    controller: emailController,
-                    type: TextInputType.emailAddress,
-                    validate: (String? value) {
-                      if (value != null &&
-                          value.isNotEmpty &&
-                          EmailValidator.validate(value)) {
-                        return null;
-                      } else {
-                        return "Please enter valid email.";
-                      }
-                    },
-                    label: 'Email Address',
-                    radius: 10,
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  defaultFormField(
-                    controller: passwordController,
-                    type: TextInputType.visiblePassword,
-                    isPassword: cubit.isPassword,
-                    validate: (String? value) {
-                      if (value != null && value.trim().length >= 3) {
-                        return null;
-                      } else {
-                        return "Please enter 3 characters at least.";
-                      }
-                    },
-                    label: 'Password',
-                    suffix: cubit.suffix,
-                    suffixPressed: () {
-                      cubit.changePasswordVisibility();
-                    },
-                    radius: 10,
-                    onSubmit: (value) {
-                      if (cubit.formKey.currentState!.validate()) {
-                        cubit.userLogin(
-                          email: emailController.text,
-                          password: passwordController.text,
-                        );
-                      }
-                    },
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Spacer(),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(
-                                context, ForgetPassword.routeName);
-                          },
-                          child: textStyle.normal('Forget Password?', 13.5)
-                          // child: Text(
-                          //   AppLocalizations.of(context)!.forgot_password,
-                          //   style: TextStyle(
-                          //     color: AppColors.primaryColor,
-                          //   ),
-                          // ),
+        return SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30),
+                  topLeft: Radius.circular(30),
+                )),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: cubit.formKey,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          child: Text(
+                            'Register',
+                            style: TextStyle(
+                              color: AppColors.disabledAndHintColor,
+                              fontSize: 14,
+                              decoration: TextDecoration.underline,
+                            ),
                           ),
-                    ],
-                  ),
-                  Spacer(),
-                  ConditionalBuilder(
-                    condition: state is! AppLoginLoadingState,
-                    builder: (context) => button(
-                      text: 'login',
-                      function: () {
+                          onPressed: () {
+                            cubit.changeBottomSheet();
+                          },
+                        ),
+                        SizedBox(
+                          width: 85.0,
+                        ),
+                        TextButton(
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: AppColors.primaryColor,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                          onPressed: () {
+                            cubit.changeBottomSheet();
+                          },
+                        ),
+                      ],
+                    ),
+                    defaultFormField(
+                      controller: emailController,
+                      type: TextInputType.emailAddress,
+                      validate: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your email address';
+                        }
+                        return null;
+                      },
+                      label: 'Email Address',
+                      prefix: Icons.email_outlined,
+                      radius: 10,
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    defaultFormField(
+                      controller: passwordController,
+                      type: TextInputType.visiblePassword,
+                      isPassword: cubit.isPassword,
+                      validate: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                      label: 'Password',
+                      suffix: cubit.suffix,
+                      suffixPressed: () {
+                        cubit.changePasswordVisibility();
+                      },
+                      prefix: Icons.lock_outlined,
+                      radius: 10,
+                      onSubmit: (value) {
                         if (cubit.formKey.currentState!.validate()) {
                           cubit.userLogin(
                             email: emailController.text,
@@ -150,10 +139,49 @@ class LoginScreen extends StatelessWidget {
                         }
                       },
                     ),
-                    fallback: (context) =>
-                        Center(child: CircularProgressIndicator()),
-                  ),
-                ],
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    ConditionalBuilder(
+                      condition: state is! AppLoginLoadingState,
+                      builder: (context) => defaultButton(
+                        background: AppColors.primaryColor,
+                        text: 'login',
+                        isUpperCase: true,
+                        function: () {
+                          if (cubit.formKey.currentState!.validate()) {
+                            cubit.userLogin(
+                              email: emailController.text,
+                              password: passwordController.text,
+                            );
+                          }
+                        },
+                        radius: 10,
+                      ),
+                      fallback: (context) =>
+                          Center(child: CircularProgressIndicator()),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            navigateTo(
+                              widget: ForgetPassword(),
+                              context: context,
+                            );
+                          },
+                          child: Text(
+                            AppLocalizations.of(context)!.forgot_password,
+                            style: TextStyle(
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
